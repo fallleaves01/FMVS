1091f06a78b99f35180f325684120829c5319411
fallleaves01 <adoubiq@qq.com>
2025-11-25 04:11:31 +0800

    feat: 添加图的存储接口

--- include/Graph.hpp
+++ include/Graph.hpp
@@ -1,5 +1,7 @@
 #pragma once
+#include <FIO.hpp>
 #include <algorithm>
+#include <iostream>
 #include <vector>
 
 class Graph {
@@ -8,6 +10,7 @@ class Graph {
         struct Edge {
             unsigned to;
             std::vector<float> alpha;
+            Edge() = default;
             Edge(unsigned t) : to(t), alpha{0, 1} {}
 
             void remove(float fl, float fr) {
@@ -36,9 +39,69 @@ class Graph {
                 }
                 return false;
             }
+
+            bool save(std::ostream& out) {
+                if (!base_write(out, to)) {
+                    return false;
+                }
+                if (!basic_vector_write(out, alpha)) {
+                    return false;
+                }
+                return true;
+            }
+            bool load(std::istream& in) {
+                if (!base_read(in, to)) {
+                    return false;
+                }
+                if (!basic_vector_read(in, alpha)) {
+                    return false;
+                }
+                return true;
+            }
         };
+
+        bool save(std::ostream& out) {
+            if (!item_vector_write(out, edges)) {
+                return false;
+            }
+            return true;
+        }
+        bool load(std::istream& in) {
+            size_t n;
+            if (!base_read(in, n)) {
+                return false;
+            }
+            if (!item_vector_read(in, edges, n)) {
+                return false;
+            }
+            return true;
+        }
+
         std::vector<Edge> edges;
     };
+
+    Graph() = default;
+    Graph(size_t n) : nodes(n) {}
+
+    Node& operator[](size_t id) { return nodes[id]; }
+    std::vector<Node::Edge>& get_edges(size_t id) { return nodes[id].edges; }
+
+    bool save(std::ostream& out) {
+        if (!item_vector_write(out, nodes)) {
+            return false;
+        }
+        return true;
+    }
+    bool load(std::istream& in) {
+        size_t n;
+        if (!base_read(in, n)) {
+            return false;
+        }
+        if (!item_vector_read(in, nodes, n)) {
+            return false;
+        }
+        return true;
+    }
 
    private:
     std::vector<Node> nodes;
