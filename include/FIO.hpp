02d7d232c42c023761a4fd24e2ca921ba19d710f
fallleaves01 <adoubiq@qq.com>
2025-11-25 04:11:08 +0800

    修改文件接口

--- include/FIO.hpp
+++ include/FIO.hpp
@@ -1,52 +1,54 @@
 #pragma once
 #include <fstream>
-#include <vector>
 #include <nlohmann/json.hpp>
+#include <vector>
 
 template <typename T>
-void base_read(std::istream &fin, T &value) {
+bool base_read(std::istream& fin, T& value) {
     fin.read(reinterpret_cast<char*>(&value), sizeof(T));
+    return fin.good();
 }
 
 template <typename T>
-void vector_read(std::istream &fin, std::vector<T> &vec) {
+bool basic_vector_read(std::istream& fin, std::vector<T>& vec) {
     size_t n;
     base_read(fin, n);
     vec.resize(n);
     fin.read(reinterpret_cast<char*>(vec.data()), n * sizeof(T));
+    return fin.good();
 }
 
 template <typename T>
-void base_write(std::ostream &fout, const T &value) {
+bool base_write(std::ostream& fout, const T& value) {
     fout.write(reinterpret_cast<const char*>(&value), sizeof(T));
+    return fout.good();
 }
 
 template <typename T>
-void vector_write(std::ostream &fout, const std::vector<T> &vec) {
+bool basic_vector_write(std::ostream& fout, const std::vector<T>& vec) {
     size_t n = vec.size();
     base_write(fout, n);
     fout.write(reinterpret_cast<const char*>(vec.data()), n * sizeof(T));
+    return fout.good();
 }
 
 template <typename T>
-void json_vector_read(std::istream &fin, std::vector<T> &vec) {
-    nlohmann::json j;
-    fin >> j;
-    vec = j.get<std::vector<T>>();
+bool item_vector_write(std::ostream& fout, const std::vector<T>& vec) {
+    for (const auto& item : vec) {
+        if (!base_write(fout, item)) {
+            return false;
+        }
+    }
+    return true;
 }
 
 template <typename T>
-void json_vector_write(std::ostream &fout, const std::vector<T> &vec) {
-    nlohmann::json j = vec;
-    fout << j;
-}
-
-template <typename T>
-void pointer_read(std::istream &fin, T* ptr, size_t length) {
-    fin.read(reinterpret_cast<char*>(ptr), length * sizeof(T));
-}
-
-template <typename T>
-void pointer_write(std::ostream &fout, const T* ptr, size_t length) {
-    fout.write(reinterpret_cast<const char*>(ptr), length * sizeof(T));
+bool item_vector_read(std::istream& fin, std::vector<T>& vec, size_t n) {
+    vec.resize(n);
+    for (size_t i = 0; i < n; ++i) {
+        if (!base_read(fin, vec[i])) {
+            return false;
+        }
+    }
+    return true;
 }
