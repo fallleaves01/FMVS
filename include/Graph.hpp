f48cdb0d40a0dbe12a1c7bfdfc39dff581679c95
fallleaves01 <adoubiq@qq.com>
2025-11-25 20:29:30 +0800

    修改了可行区间的格式

--- include/Graph.hpp
+++ include/Graph.hpp
@@ -9,38 +9,38 @@ class Graph {
     struct Node {
         struct Edge {
             unsigned to;
-            std::vector<float> alpha;
+            std::vector<std::pair<float, float>> alpha;
             Edge() = default;
-            Edge(unsigned t) : to(t), alpha{0, 1} {}
+            Edge(unsigned t) : to(t), alpha{std::pair{0.0f, 1.0f}} {}
 
             void remove(float fl, float fr) {
-                size_t id = std::lower_bound(alpha.begin(), alpha.end(), fl) -
-                            alpha.begin();
-                if (id == alpha.size()) {
-                    return;
+                auto it = std::ranges::lower_bound(alpha, std::pair{fl, -1.0f});
+                if (it != alpha.begin() && prev(it)->second > fl) {
+                    it--;
                 }
-                if (id % 2 == 1) {
-                    alpha[id] = fl;
-                    id++;
+                while (it != alpha.end() && it->first < fr) {
+                    if (it->first < fl && it->second > fr) {
+                        auto [l, r] = *it;
+                        it = alpha.erase(it);
+                        it = alpha.insert(it, std::pair{fr, r});
+                        alpha.insert(it, std::pair{l, fl});
+                        break;
+                    } else if (it->first >= fl && it->second <= fr) {
+                        it = alpha.erase(it);
+                    } else if (it->first >= fl) {
+                        it->first = std::max(it->first, fr), ++it;
+                    } else {
+                        it->second = std::min(it->second, fl), ++it;
+                    }
                 }
-                while (id + 1 < alpha.size() && alpha[id + 1] <= fr) {
-                    alpha.erase(alpha.begin() + id, alpha.begin() + id + 2);
-                }
-                alpha[id] = std::max(alpha[id], fr);
             }
             bool empty() const { return alpha.empty(); }
-            bool valid(float alpha) const {
-                size_t id = std::lower_bound(this->alpha.begin(),
-                                             this->alpha.end(), alpha) -
-                            this->alpha.begin();
-                if (id % 2 == 1 ||
-                    (id < this->alpha.size() && this->alpha[id] == alpha)) {
-                    return true;
-                }
-                return false;
+            bool valid(float a) const {
+                auto it = std::ranges::upper_bound(alpha, std::pair{a, -1.0f});
+                return it != alpha.begin() && std::prev(it)->second > a;
             }
 
-            bool save(std::ostream& out) {
+            bool save(std::ostream& out) const {
                 if (!base_write(out, to)) {
                     return false;
                 }
@@ -60,7 +60,7 @@ class Graph {
             }
         };
 
-        bool save(std::ostream& out) {
+        bool save(std::ostream& out) const {
             if (!item_vector_write(out, edges)) {
                 return false;
             }
@@ -86,7 +86,7 @@ class Graph {
     Node& operator[](size_t id) { return nodes[id]; }
     std::vector<Node::Edge>& get_edges(size_t id) { return nodes[id].edges; }
 
-    bool save(std::ostream& out) {
+    bool save(std::ostream& out) const {
         if (!item_vector_write(out, nodes)) {
             return false;
         }
